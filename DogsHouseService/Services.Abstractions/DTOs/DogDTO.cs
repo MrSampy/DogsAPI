0135d549c9e7aee4b20416747859ecceb0043653
serhii <mynameisserzheo@gmail.com>
2024-10-31 20:52:59 +0200

    Add DogDTO

---
+++ DogsHouseService/Services.Abstractions/DTOs/DogDTO.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Services.Abstractions.DTOs
+{
+    public class DogDTO
+    { 
+        public string Name { get; set; }
+        public string Color { get; set; }
+        [JsonPropertyName("tail_length")]
+        public int TailLength { get; set; }
+        public int Weight { get; set; }        
+    }
+}
