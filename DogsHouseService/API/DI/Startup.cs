48fbe39ccaf38da9d650b8c477863d030f765d2a
serhii <mynameisserzheo@gmail.com>
2024-10-31 20:59:04 +0200

    Add Startup file

---
+++ DogsHouseService/API/DI/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Services.Mappers;
+using API.Middleware;
+using API.Extensions;
+
+namespace API.DI
+{
+    internal sealed class Startup(IConfiguration configuration)
+    {
+        public IConfiguration Configuration { get; } = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRepositoryDbContext(Configuration);
+
+            services.AddRepositories();
+
+            services.AddServices();
+
+            services.AddAutoMapper(typeof(AutomapperProfile).Assembly);
+
+            services.AddControllers();
+
+            services.AddDistributedMemoryCache();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+            app.UseMiddleware<RateLimitMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
