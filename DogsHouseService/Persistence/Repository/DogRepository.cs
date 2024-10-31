b1266cea493bd80d66fc9cba9801c18d0ded7bf2
serhii <mynameisserzheo@gmail.com>
2024-10-31 20:57:35 +0200

    Add RepositoryDbContext for database configuration

---
+++ DogsHouseService/Persistence/Repository/DogRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Persistence.Repository
+{
+    public sealed class DogRepository(RepositoryDbContext dbContexts) : IDogRepository
+    {
+        private readonly RepositoryDbContext _dbContexts = dbContexts;
+
+        public async Task<List<Dog>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContexts.Dogs.AsNoTracking().ToListAsync(cancellationToken);
+        }
+        public async Task<Dog?> FindByConditionAsync(Expression<Func<Dog, bool>> expression, CancellationToken cancellationToken = default)
+        {
+            return await _dbContexts.Dogs.AsNoTracking().FirstOrDefaultAsync(expression, cancellationToken);
+        }
+        public void Insert(Dog dog)
+        {
+            _dbContexts.Dogs.Add(dog);
+        }
+    }
+}
