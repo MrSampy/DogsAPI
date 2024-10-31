a6776cd9a9fd5169a4ad416d66ddf69010e08050
serhii <mynameisserzheo@gmail.com>
2024-10-31 20:58:11 +0200

    Add UnitOfWork for transaction management

---
+++ DogsHouseService/Persistence/DbContexts/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using Persistence.DbContexts;
+using Persistence.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.DbContexts
+{
+    public sealed class UnitOfWork(RepositoryDbContext dbContext) : IUnitOfWork
+    {
+        private readonly RepositoryDbContext dbContext = dbContext;
+
+        private DogRepository _dogRepository;
+        public IDogRepository DogRepository => _dogRepository ??= new DogRepository(dbContext);
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
