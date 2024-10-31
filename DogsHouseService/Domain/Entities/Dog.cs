fd6bcdf2f65f2e1529cb681839028c5ee1b61436
serhii <mynameisserzheo@gmail.com>
2024-10-31 20:51:35 +0200

    Add Dog entity to Domain

---
+++ DogsHouseService/Domain/Entities/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Dog
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        public string Color { get; set; }
+        [Required]
+        [Column("tail_length")]
+        public int TailLength { get; set; }
+        [Required]
+        public int Weight { get; set; }
+    }
+}
