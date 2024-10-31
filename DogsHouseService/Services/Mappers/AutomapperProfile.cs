720e5d1af666ee417997d1a730f3b7f7ccd59c59
serhii <mynameisserzheo@gmail.com>
2024-10-31 20:54:01 +0200

    Add AutoMapper profile for Dog mappings

---
+++ DogsHouseService/Services/Mappers/AutomapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Services.Abstractions.DTOs;
+
+namespace Services.Mappers
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<DogDTO, Dog>()
+                .ForMember(dest => dest.Color, opt => opt.MapFrom(src => src.Color.Replace("&", " & ")));
+            CreateMap<Dog, DogDTO>()
+                .ForMember(dest => dest.Color, opt => opt.MapFrom(src => src.Color.Replace(" & ", "&")));
+        }
+    }
+}
